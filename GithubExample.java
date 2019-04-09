3cbfb5af148a22154718924efbd16f444d4b0282
sswang39 <stevewang@stanfordalumni.org>
2019-04-09 14:45:39 -0400

    Update GithubExample.java

--- GithubExample.java
+++ GithubExample.java
@@ -11,7 +11,8 @@ public class GithubExample {
 		System.out.println("Let's calculate distance with this simple equation:");
 		System.out.println();
 		Scanner s = new Scanner(System.in);
-		System.out.println("Enter  Velocity :");
+		System.out.println("Enter  Velocity, which is the fancy way to say speed :");
+		// trying again to force a conflict message
 		double velocity = s.nextDouble();
 		System.out.println("Enter time:");
 		double time = s.nextDouble();
