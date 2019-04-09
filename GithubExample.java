385bfb5efafeed6697732a4ce025ee2ffb2d7aad
sswang39 <stevewang@stanfordalumni.org>
2019-04-09 13:51:55 -0400

    Add files via upload

---
+++ GithubExample.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+public class GithubExample {
+	
+	public static void main(String[] args) {
+		/*
+		 * Penn MCIT Week 11
+		 * Github 
+		 */
+		
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter velocity:");
+		double velocity = s.nextDouble();
+		System.out.println("Enter time:");
+		double time = s.nextDouble();
+		double distance = velocity * time;
+		System.out.println();
+		System.out.println("Distance is " + distance);
+	}
+
+}
