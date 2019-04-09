9fdf81fe84a1c431a317655568479b6463a476db
Steven Wang <stevenwang@Stevens-MacBook-Pro.local>
2019-04-09 15:35:31 -0400

    trying to generate a conflict

--- GithubExample.java
+++ GithubExample.java
@@ -8,13 +8,11 @@ public class GithubExample {
 		 * Github 
 		 */
 		
-		System.out.println("Let's calculate distance with this simple equation:");
-		System.out.println();
 		Scanner s = new Scanner(System.in);
-		System.out.println("Enter  Velocity, which is the fancy way to say speed :");
-		// trying again to force a conflict message
+		System.out.println("Enter velocity, which is a fancy way of saying speed:");
 		double velocity = s.nextDouble();
 		System.out.println("Enter time:");
+		System.out.println("Of course, time is relative and dilates with speed.");
 		double time = s.nextDouble();
 		double distance = velocity * time;
 		System.out.println();
